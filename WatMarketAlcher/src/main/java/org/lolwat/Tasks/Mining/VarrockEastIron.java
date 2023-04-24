c2db5372d7ca297de401e104d710118d5cdfceea
lolwat <me@lolwat.net>
2023-04-23 23:16:08 -0400

    Cleaned project

--- WatMarketAlcher/src/main/java/org/lolwat/Tasks/Mining/VarrockEastIron.java
+++ WatMarketAlcher/src/main/java/org/lolwat/Tasks/Mining/VarrockEastIron.java
@@ -1,5 +1,6 @@
 package org.lolwat.Tasks.Mining;
 
+import org.dreambot.api.methods.map.Tile;
 import org.dreambot.api.methods.quest.book.Quest;
 import org.dreambot.api.methods.skills.Skill;
 import org.lolwat.Tasks.WatTask;
@@ -10,6 +11,12 @@ import java.util.List;
 
 public class VarrockEastIron implements WatTask {
     String name = "Varrock East Iron";
+
+    private Tile varrockEastMine = new Tile(3286, 3368);
+    private Tile varrockEastAlt = new Tile(3285, 3370);
+
+    private List<Tile> varrockEastRocks;
+    private List<Tile> varrockEastAltRocks;
 
     HashMap<Skill, Integer> levelRequirements = new HashMap<Skill, Integer>() {
         {
