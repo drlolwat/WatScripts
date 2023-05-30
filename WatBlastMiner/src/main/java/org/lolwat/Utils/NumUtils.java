b069c1a3e793ab9203896af322c0485eaec78a9e
lolwat <me@lolwat.net>
2023-05-30 19:40:41 -0400

    paint

---
+++ WatBlastMiner/src/main/java/org/lolwat/Utils/NumUtils.java
@@ -0,0 +1,13 @@
+package org.lolwat.Utils;
+
+public class NumUtils {
+    public static String simplifyNumber(double number) {
+        if (number >= 1000000) {
+            return String.format("%.2fM", number / 1000000);
+        } else if (number >= 1000) {
+            return String.format("%.2fK", number / 1000);
+        } else {
+            return String.format("%.2f", number);
+        }
+    }
+}
