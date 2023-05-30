70f660da8f7fb58b0ae4ade8fa81f4375d4c32bb
lolwat <me@lolwat.net>
2023-05-30 19:40:41 -0400

    paint

---
+++ WatAB/src/main/java/org/lolwat/Utils/SkillUtils.java
@@ -0,0 +1,30 @@
+package org.lolwat.Utils;
+
+import org.dreambot.api.methods.skills.Skill;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SkillUtils {
+    public static boolean isFreeToPlay(Skill skill) {
+        List<Skill> f2pSkills = Arrays.asList(
+                Skill.ATTACK,
+                Skill.STRENGTH,
+                Skill.DEFENCE,
+                Skill.RANGED,
+                Skill.PRAYER,
+                Skill.MAGIC,
+                Skill.RUNECRAFTING,
+                Skill.HITPOINTS,
+                Skill.COOKING,
+                Skill.WOODCUTTING,
+                Skill.FISHING,
+                Skill.FIREMAKING,
+                Skill.CRAFTING,
+                Skill.SMITHING,
+                Skill.MINING
+        );
+
+        return f2pSkills.contains(skill);
+    }
+}
