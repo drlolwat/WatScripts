c7890e00b5bd57ba0cceb1b4834379d2556692d1
lolwat <me@lolwat.net>
2023-05-31 22:28:45 -0400

    Smithing task will now buy missing ores

--- WatWcGuild/src/main/java/org/lolwat/Utils/ItemUtils.java
+++ WatWcGuild/src/main/java/org/lolwat/Utils/ItemUtils.java
@@ -23,41 +23,41 @@ public class ItemUtils {
         }
     }
 
-    public static HashMap<String, Integer> getMaterialsForBar(IngotType type, boolean fullInventory) {
+    public static HashMap<String, Integer> getMaterialsForBar(IngotType type, boolean fullInventory, int inventoryLoads) {
         HashMap<String, Integer> ret = new HashMap<>();
         switch(type) {
             default: return ret;
             case BRONZE: {
-                ret.put("Copper ore", fullInventory ? 14 : 1);
-                ret.put("Tin ore", fullInventory ? 14 : 1);
+                ret.put("Copper ore", fullInventory ? 14 * inventoryLoads : 1);
+                ret.put("Tin ore", fullInventory ? 14 * inventoryLoads : 1);
                 break;
             }
             case IRON: {
-                ret.put("Iron ore", fullInventory? 28 : 1);
+                ret.put("Iron ore", fullInventory? 28 * inventoryLoads : 1);
                 break;
             }
             case STEEL: {
-                ret.put("Iron ore", fullInventory ? 9 : 1);
-                ret.put("Coal", fullInventory ? 18 : 2);
+                ret.put("Iron ore", fullInventory ? 9 * inventoryLoads : 1);
+                ret.put("Coal", fullInventory ? 18 * inventoryLoads : 2);
                 break;
             }
             case GOLD: {
-                ret.put("Gold ore", fullInventory ? 28 : 1);
+                ret.put("Gold ore", fullInventory ? 28 * inventoryLoads : 1);
                 break;
             }
             case MITHRIL: {
-                ret.put("Mithril ore", fullInventory ? 5 : 1);
-                ret.put("Coal", fullInventory ? 20 : 4);
+                ret.put("Mithril ore", fullInventory ? 5 * inventoryLoads : 1);
+                ret.put("Coal", fullInventory ? 20 * inventoryLoads : 4);
                 break;
             }
             case ADAMANT: {
-                ret.put("Adamantite ore", fullInventory ? 4 : 1);
-                ret.put("Coal", fullInventory ? 24 : 6);
+                ret.put("Adamantite ore", fullInventory ? 4 * inventoryLoads : 1);
+                ret.put("Coal", fullInventory ? 24 * inventoryLoads : 6);
                 break;
             }
             case RUNE: {
-                ret.put("Runite ore", fullInventory ? 3 : 1);
-                ret.put("Coal", fullInventory ? 24 : 8);
+                ret.put("Runite ore", fullInventory ? 3 * inventoryLoads : 1);
+                ret.put("Coal", fullInventory ? 24 * inventoryLoads : 8);
                 break;
             }
         }
