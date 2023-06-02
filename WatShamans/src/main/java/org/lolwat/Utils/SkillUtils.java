f180c55448348ee2de78392e5c6a1bd79555235a
lolwat <me@lolwat.net>
2023-06-02 00:31:58 -0400

    Fix banking task buying the correct amount when it already owns a partial qty of an item. Fishing task completed and several locations added Grand exchange task updated to set the buy price to 10gp for items <= 7gp Bank task set to withdraw the correct amount for items <= 7gp (>=8gp) Fishing helper methods

--- WatShamans/src/main/java/org/lolwat/Utils/SkillUtils.java
+++ WatShamans/src/main/java/org/lolwat/Utils/SkillUtils.java
@@ -1,9 +1,11 @@
 package org.lolwat.Utils;
 
 import org.dreambot.api.methods.skills.Skill;
+import org.lolwat.Enums.FishType;
 import org.lolwat.Enums.OreType;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class SkillUtils {
@@ -38,5 +40,45 @@ public class SkillUtils {
             case ADAMANTITE: return 70;
             case RUNITE: return 85;
         }
+    }
+
+    public static String getToolByFishType(FishType type) {
+        switch(type) {
+            default: return null; //return net later
+            case SHRIMP: return "Small fishing net";
+            case HERRING:
+            case PIKE: return "Fishing rod";
+            case TUNA:
+            case SWORDFISH: return "Harpoon";
+            case SALMON: return "Fly fishing rod";
+            case LOBSTER: return "Lobster pot";
+        }
+    }
+
+    public static String getMenuItemByFishType(FishType type) {
+        switch(type) {
+            default: return null;
+            case SHRIMP: return "Net";
+            case HERRING:
+            case PIKE: return "Bait";
+            case TUNA:
+            case SWORDFISH: return "Harpoon";
+            case SALMON: return "Lure";
+            case LOBSTER: return "Cage";
+        }
+    }
+
+    public static HashMap<String, Integer> getExtraFishingItems(FishType type) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        switch(type) {
+            default: break;
+            case LOBSTER: map.put("Coins", 25); break;
+            case HERRING:
+            case PIKE: map.put("Fishing bait", 1000); break;
+            case SALMON: map.put("Feather", 1000); break;
+        }
+
+        return map;
     }
 }
