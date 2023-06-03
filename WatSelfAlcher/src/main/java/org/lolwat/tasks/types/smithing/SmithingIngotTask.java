f80203dcdb7f887ab74a5a4bfe13bf391441052f
lolwat <me@lolwat.net>
2023-06-03 11:08:05 -0400

    Major restructure/refactor Spinning task coded and tested

---
+++ WatSelfAlcher/src/main/java/org/lolwat/tasks/types/smithing/SmithingIngotTask.java
@@ -0,0 +1,128 @@
+package org.lolwat.tasks.types.smithing;
+
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.dialogues.Dialogues;
+import org.dreambot.api.methods.input.Camera;
+import org.dreambot.api.methods.interactive.GameObjects;
+import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.methods.map.Tile;
+import org.dreambot.api.methods.quest.book.Quest;
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.skills.Skills;
+import org.dreambot.api.methods.map.Map;
+import org.dreambot.api.methods.widget.Widgets;
+import org.dreambot.api.utilities.Sleep;
+import org.lolwat.utils.types.IngotType;
+import org.lolwat.tasks.types.misc.BankingTask;
+import org.lolwat.tasks.types.misc.TraversalTask;
+import org.lolwat.tasks.WatTask;
+import org.lolwat.utils.ItemUtils;
+import org.lolwat.utils.WidgetUtils;
+import org.lolwat.WatAIO;
+
+import java.util.*;
+
+public class SmithingIngotTask implements WatTask {
+    private final List<Tile> furnaceLocations = Collections.singletonList(new Tile(3107, 3499));
+    private final HashMap<Skill, Integer> levelRequirements = new HashMap<>();
+    private final Tile selectedLocation;
+    private final IngotType smithingType;
+    private final int avoidAtLevel;
+    private final HashMap<String, Integer> toSell;
+    private long cooldown;
+    private final int totalLoads;
+
+    public SmithingIngotTask(IngotType type, int smithingLevel, int pAvoidAtLevel, HashMap<String, Integer> sellList) {
+        setRequirements(new HashMap<Skill, Integer>() {{
+            put(Skill.SMITHING, smithingLevel);
+        }}, new ArrayList<>());
+
+        selectedLocation = furnaceLocations.get(new Random().nextInt(furnaceLocations.size()));
+        smithingType = type;
+        avoidAtLevel = pAvoidAtLevel;
+        toSell = sellList;
+        cooldown = 0;
+        totalLoads = Calculations.random(10, 15);
+    }
+
+    @Override
+    public void execute(WatAIO instance) {
+        for(java.util.Map.Entry<String, Integer> m : ItemUtils.getMaterialsForBar(smithingType, false, 1).entrySet()) {
+            // do we have enough to create at least 1 bar of this type?
+            if(!Inventory.contains(m.getKey()) || Inventory.get(m.getKey()).getAmount() < m.getValue()) {
+                instance.currentTask = new BankingTask("Grabbing ore",
+                        ItemUtils.getMaterialsForBar(smithingType, true, 1),
+                        true, this, true, toSell, totalLoads);
+
+                return;
+            }
+        }
+
+        if(!Map.isTileOnScreen(selectedLocation)) {
+            if(Map.isTileOnMap(selectedLocation) && Map.exactDistance(selectedLocation) <= 6) {
+                Camera.rotateToTile(selectedLocation);
+                return;
+            }
+
+            instance.currentTask = new TraversalTask(selectedLocation, false, this);
+            return;
+        }
+
+        if(GameObjects.closest("Furnace") != null && !Players.getLocal().isAnimating()) {
+            if(GameObjects.closest("Furnace").interact()) {
+                Sleep.sleepUntil(() -> Widgets.getWidget(270) != null && Widgets.getWidget(270).isVisible(), 10000);
+
+                if(Widgets.getWidget(270) != null && Widgets.getWidget(270).isVisible()) {
+                    Widgets.getWidget(270).getChild(WidgetUtils.getIngotWidgetId(smithingType)).interact();
+                    Sleep.sleepUntil(() -> cooldown > 5 || Dialogues.canContinue() || !Inventory.contains(n -> n.getName().contains("ore")), 30000);
+                }
+            }
+        }
+    }
+
+    public void setRequirements(HashMap<Skill, Integer> skills, List<Quest> quests) {
+        levelRequirements.putAll(skills);
+    }
+
+    @Override
+    public String getName() {
+        return "Smelting " + smithingType.toString().toLowerCase() + " bars";
+    }
+
+    @Override
+    public boolean canPerformTask() {
+        for (java.util.Map.Entry<Skill, Integer> map : levelRequirements.entrySet()) {
+            if (map.getValue() > Skills.getRealLevel(map.getKey())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean requiresLogin() {
+        return true;
+    }
+
+    @Override
+    public int loopTime() {
+        return 650;
+    }
+
+    @Override
+    public void onExpGained(Skill skill, int amount, WatAIO instance) {
+        cooldown = 0;
+    }
+
+    @Override
+    public Skill trainsSkill() {
+        return Skill.SMITHING;
+    }
+
+    @Override
+    public Integer avoidAfterLevel() {
+        return avoidAtLevel;
+    }
+}
---
+++ WatSelfAlcher/src/main/java/org/lolwat/utils/types/CraftingType.java
@@ -0,0 +1,11 @@
+package org.lolwat.utils.types;
+
+public enum CraftingType {
+    WOOL,
+    GOLDWOOL,
+    BOWSTRING,
+    ROPE,
+    CROSSBOWSTRING_S,
+    CROSSBOWSTRING_TR,
+    MAGICSTRING
+}
