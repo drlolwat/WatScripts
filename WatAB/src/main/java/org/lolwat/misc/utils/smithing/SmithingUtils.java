ab8754eb1529a0d6f1d07b08c4ff771b3b82eb6e
lolwat <me@lolwat.net>
2023-06-05 01:56:40 -0400

    Smithing from 1-99 using bars and a hammer. Gets pretty expensive after lv 48. Fixes for hammer+mould problems, checking it separately now and ignoring it in the Bank task if it's part of a crafting/smithing request

--- WatAB/src/main/java/org/lolwat/misc/utils/smithing/SmithingUtils.java
+++ WatAB/src/main/java/org/lolwat/misc/utils/smithing/SmithingUtils.java
@@ -1,6 +1,10 @@
 package org.lolwat.misc.utils.smithing;
 
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.skills.Skills;
 import org.lolwat.misc.types.smithing.IngotType;
+import org.lolwat.misc.types.smithing.SmithingType;
+import org.lolwat.misc.utils.StringUtils;
 
 import java.util.HashMap;
 
@@ -36,18 +40,82 @@ public class SmithingUtils {
                 ret.put("Coal", fullInventory ? 20 * inventoryLoads : 4);
                 break;
             }
-            case ADAMANT: {
+            case ADAMANTITE: {
                 ret.put("Adamantite ore", fullInventory ? 4 * inventoryLoads : 1);
                 ret.put("Coal", fullInventory ? 24 * inventoryLoads : 6);
                 break;
             }
-            case RUNE: {
+            case RUNITE: {
                 ret.put("Runite ore", fullInventory ? 3 * inventoryLoads : 1);
                 ret.put("Coal", fullInventory ? 24 * inventoryLoads : 8);
                 break;
             }
         }
         return ret;
+    }
+
+    public static HashMap<String, Integer> materialsForSmithing(SmithingType type, IngotType barType, boolean fullInventory, int inventoryLoads) {
+        String barName = StringUtils.capitalize(barType.toString().toLowerCase()) + " bar";
+        HashMap<String, Integer> ret = new HashMap<>();
+        switch(type) {
+            default:
+            case AXE: {
+                ret.put(barName, fullInventory ? 27 * inventoryLoads : 1);
+                break;
+            }
+            case SCIMITAR: {
+                ret.put(barName, fullInventory ? 26 * inventoryLoads : 2);
+                break;
+            }
+            case PLATELEGS:
+            case WARHAMMER: {
+                ret.put(barName, fullInventory ? 27 * inventoryLoads : 3);
+                break;
+            }
+            case PLATEBODY: {
+                ret.put(barName, fullInventory ? 25 * inventoryLoads : 5);
+                break;
+            }
+        }
+
+        return ret;
+    }
+
+    public static SmithingType getBestSmithingChoice(IngotType type) {
+        int smithingLevel = Skills.getRealLevel(Skill.SMITHING);
+
+        if(type == IngotType.BRONZE) {
+            if(smithingLevel >= 18) {
+                return SmithingType.PLATEBODY;
+            }
+            else if(smithingLevel >= 9) {
+                return SmithingType.WARHAMMER;
+            }
+            else if(smithingLevel >= 5) {
+                return SmithingType.SCIMITAR;
+            }
+            else {
+                return SmithingType.AXE;
+            }
+        }
+        else if(type == IngotType.IRON) {
+            return SmithingType.PLATEBODY;
+        }
+        else if(type == IngotType.STEEL) {
+            return SmithingType.PLATEBODY;
+        }
+        else if(type == IngotType.MITHRIL) {
+            return SmithingType.PLATEBODY;
+        }
+        else if(type == IngotType.ADAMANTITE) {
+            return SmithingType.PLATEBODY;
+        }
+        else if(type == IngotType.RUNITE) {
+            return SmithingType.PLATELEGS;
+        }
+        else {
+            return SmithingType.AXE;
+        }
     }
 
     public static int getIngotWidgetId(IngotType type) {
@@ -58,8 +126,8 @@ public class SmithingUtils {
             case STEEL: return 17;
             case GOLD: return 18;
             case MITHRIL: return 19;
-            case ADAMANT: return 20;
-            case RUNE: return 21;
+            case ADAMANTITE: return 20;
+            case RUNITE: return 21;
         }
     }
 }
