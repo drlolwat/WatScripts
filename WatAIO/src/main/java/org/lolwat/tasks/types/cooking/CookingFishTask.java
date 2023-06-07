a57cef5eb4677194d6e4571d4f33c50eb77353f1
lolwat <me@lolwat.net>
2023-06-07 13:07:32 -0400

    fixes

--- WatAIO/src/main/java/org/lolwat/tasks/types/cooking/CookingFishTask.java
+++ WatAIO/src/main/java/org/lolwat/tasks/types/cooking/CookingFishTask.java
@@ -77,10 +77,14 @@ public class CookingFishTask implements WatTask {
             return;
         }
 
-        if(GameObjects.closest(x -> x != null && x.getName().toLowerCase().contains("range")).interact()) {
-            Sleep.sleepUntil(() -> Widgets.getWidget(270) != null && Widgets.getWidget(270).isVisible(), 10000);
-            if(Widgets.getWidget(270).getChild(14) != null && Widgets.getWidget(270).getChild(14).interact()) {
-                Sleep.sleepUntil(() -> !Inventory.contains("Raw " + fishType.toString().toLowerCase()) || Dialogues.canContinue(), 60000);
+        List<String> objs = Arrays.asList("range", "stove", "fire");
+
+        for(String n : objs) {
+            if (GameObjects.closest(x -> x != null && x.getName().toLowerCase().contains(n)).interact()) {
+                Sleep.sleepUntil(() -> Widgets.getWidget(270) != null && Widgets.getWidget(270).isVisible(), 10000);
+                if (Widgets.getWidget(270).getChild(14) != null && Widgets.getWidget(270).getChild(14).interact()) {
+                    Sleep.sleepUntil(() -> !Inventory.contains("Raw " + fishType.toString().toLowerCase()) || Dialogues.canContinue(), 60000);
+                }
             }
         }
     }
