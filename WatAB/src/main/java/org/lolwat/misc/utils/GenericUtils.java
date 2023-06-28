4b6dda5319b5b1126cb12b69512616702d540bee
lolwat <me@lolwat.net>
2023-06-28 03:54:50 -0400

    see google

---
+++ WatAB/src/main/java/org/lolwat/misc/utils/GenericUtils.java
@@ -0,0 +1,24 @@
+package org.lolwat.misc.utils;
+
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.utilities.Logger;
+import org.dreambot.api.utilities.Sleep;
+import org.dreambot.api.wrappers.items.Item;
+
+public class GenericUtils {
+    public static void equipItem(String item) {
+        Item i = Inventory.get(item);
+        if(i != null) {
+            if(i.hasAction("Wear") && i.interact("Wear")) {
+                Logger.log("EQUIPMENTCHECKER: EQUIPPED WEARABLE");
+                Sleep.sleep(200, 400);
+            }
+            else if(i.hasAction("Wield") && i.interact("Wield")) {
+                Logger.log("EQUIPMENTCHECKER: EQUIPPED WEAPON/AMMO");
+                Sleep.sleep(200, 400);
+            }
+        } else {
+            Logger.error("item to equip was null");
+        }
+    }
+}
