b824c292e0999f686eb6301a2cb13fae65355784
lolwat <me@lolwat.net>
2023-07-03 16:13:04 -0400

    see google

--- WatScriptStarter/src/main/java/org/lolwat/tasks/types/combat/RangedCombatTask.java
+++ WatScriptStarter/src/main/java/org/lolwat/tasks/types/combat/RangedCombatTask.java
@@ -148,7 +148,7 @@ public class RangedCombatTask implements WatTask {
 
         if (!Players.getLocal().isInCombat()) {
             NPC closestFriend = NPCs.closest(x -> x != null && x.exists() && x.getName().equalsIgnoreCase(name) && !x.isInCombat() && !x.isHealthBarVisible() && zone.contains(x));
-            if (closestFriend != null && !closestFriend.isInCombat() && !closestFriend.isHealthBarVisible() && closestFriend.interact()) {
+            if (closestFriend != null && !closestFriend.isInCombat() && !closestFriend.isHealthBarVisible() && closestFriend.interact("Attack")) {
                 Mouse.move();
                 //Sleep.sleepUntil(() -> !closestFriend.exists() || closestFriend.isHealthBarVisible(), 5000);
             }
