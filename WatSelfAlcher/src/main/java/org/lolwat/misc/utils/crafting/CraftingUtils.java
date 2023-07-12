38e814edbbfa5a4e44d73d5a71b5e0d50033d81a
lolwat <me@lolwat.net>
2023-07-12 01:28:19 -0400

    Jeweled amulets, rings. Muling/Reverse fixes. Selling fixes.

--- WatSelfAlcher/src/main/java/org/lolwat/misc/utils/crafting/CraftingUtils.java
+++ WatSelfAlcher/src/main/java/org/lolwat/misc/utils/crafting/CraftingUtils.java
@@ -17,19 +17,46 @@ public class CraftingUtils {
 
     public static HashMap<String, Integer> getMaterialsForJewelry(CraftingType type, boolean fullInventory, int inventoryLoads) {
         HashMap<String, Integer> ret = new HashMap<>();
-        String extra = StringUtils.capitalize(type.toString().toLowerCase()) + " mould";
-        ret.put(extra, 1);
+
+        String mouldType;
+
+        if(type.toString().toLowerCase().contains("ring")) {
+            mouldType = "Ring";
+        } else {
+            mouldType = "Amulet";
+        }
 
         switch(type) {
+            case AMULET:
             case RING: {
                 ret.put("Gold bar", fullInventory ? 27 * inventoryLoads : 1);
                 break;
             }
 
-            case AMULET: {
-                ret.put("Gold bar", fullInventory ? 27 * inventoryLoads : 1);
+            case SAPPHIRERING: {
+                ret.put("Gold bar", fullInventory ? 13 * inventoryLoads : 1);
+                ret.put("Sapphire", fullInventory ? 13 * inventoryLoads : 1);
+                break;
             }
+
+            case DIAMONDAMULET:
+            case EMERALDAMULET:
+            case EMERALDRING: {
+                ret.put("Gold bar", fullInventory ? 13 * inventoryLoads : 1);
+                ret.put("Emerald", fullInventory ? 13 * inventoryLoads : 1);
+                break;
+            }
+
+            case RUBYAMULET: {
+                ret.put("Gold bar", fullInventory ? 13 * inventoryLoads : 1);
+                ret.put("Ruby", fullInventory ? 13 * inventoryLoads : 1);
+                break;
+            }
+
         }
+
+        String extra = mouldType + " mould";
+        ret.put(extra, 1);
 
         return ret;
     }
@@ -50,6 +77,11 @@ public class CraftingUtils {
     public static int getJewelryParentId(CraftingType type) {
         switch(type) {
             default:
+            case SAPPHIRERING:
+            case EMERALDRING:
+            case EMERALDAMULET:
+            case RUBYAMULET:
+            case DIAMONDAMULET:
             case AMULET:
             case RING: return 446;
         }
@@ -59,7 +91,12 @@ public class CraftingUtils {
         switch(type) {
             default:
             case RING: return 8;
+            case SAPPHIRERING: return 9;
+            case EMERALDRING: return 10;
             case AMULET: return 37;
+            case EMERALDAMULET: return 39;
+            case RUBYAMULET: return 40;
+            case DIAMONDAMULET: return 41;
         }
     }
 }
