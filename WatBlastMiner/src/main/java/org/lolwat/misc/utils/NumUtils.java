5e429d9788459e83f2b148251c5f745245e32b2b
lolwat <me@lolwat.net>
2023-07-23 04:01:55 -0400

    Anti-ban via cloud-based profiles. More dynamic behavior.

--- WatBlastMiner/src/main/java/org/lolwat/misc/utils/NumUtils.java
+++ WatBlastMiner/src/main/java/org/lolwat/misc/utils/NumUtils.java
@@ -1,5 +1,7 @@
 package org.lolwat.misc.utils;
 
+import org.dreambot.api.methods.grandexchange.LivePrices;
+
 public class NumUtils {
     public static String simplifyNumber(double number) {
         if (number >= 1000000) {
@@ -9,5 +11,28 @@ public class NumUtils {
         } else {
             return String.format("%.2f", number);
         }
+    }
+
+    public static int getItemPrice(String item) {
+        int livePrice = LivePrices.get(item);
+        int ret = 1;
+
+        if(livePrice > 0) {
+            if(livePrice >= 10000) {
+                ret = (int) (livePrice * 1.3);
+            }
+            else if(livePrice >= 5000) {
+                ret = (int) (livePrice * 1.8);
+            }
+            else {
+                if (livePrice <= 1000) {
+                    ret = livePrice * 2;
+                } else {
+                    ret = (int) (livePrice * 1.8);
+                }
+            }
+        }
+
+        return (int) (ret * 1.2);
     }
 }
