cb7f8d266649017ebaeea74ee75f0f492bcfea1e
lolwat <chchproud@gmail.com>
2023-11-12 14:49:44 -0500

    Added clothing/stylescape to all non-combat tasks

--- WatBlastMiner/src/main/java/org/lolwat/tasks/types/quests/ImpCatcherQuest.java
+++ WatBlastMiner/src/main/java/org/lolwat/tasks/types/quests/ImpCatcherQuest.java
@@ -12,6 +12,7 @@ import org.dreambot.api.methods.quest.book.Quest;
 import org.dreambot.api.methods.skills.Skill;
 import org.lolwat.WatAIO;
 import org.lolwat.misc.utils.DialogueUtils;
+import org.lolwat.misc.utils.GenericUtils;
 import org.lolwat.tasks.WatTask;
 import org.lolwat.tasks.types.misc.BankingTask;
 import org.lolwat.tasks.types.misc.TraversalTask;
@@ -113,6 +114,6 @@ public class ImpCatcherQuest implements WatTask {
 
     @Override
     public HashMap<String, Integer> clothesRequired() {
-        return new HashMap<>();
+        return GenericUtils.styleScape();
     }
 }
