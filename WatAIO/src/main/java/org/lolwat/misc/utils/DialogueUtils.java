781b19e30814ca4739ec752c95247d7cde9732d8
lolwat <chchproud@gmail.com>
2023-11-25 03:25:59 -0500

    Tutorial Island Revamp 90%

--- WatAIO/src/main/java/org/lolwat/misc/utils/DialogueUtils.java
+++ WatAIO/src/main/java/org/lolwat/misc/utils/DialogueUtils.java
@@ -9,6 +9,8 @@ import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.utilities.Sleep;
 import org.dreambot.api.wrappers.interactive.NPC;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -26,7 +28,7 @@ public class DialogueUtils {
 
     // Credit to the DreamBot forums for this solution
     public static void solve(List<String> answers) {
-        if(Dialogues.getOptions() != null) {
+        if(Dialogues.getOptions() != null && !answers.isEmpty()) {
             final List<String> options = Arrays.asList(Dialogues.getOptions());
             final Optional<String> optionalOption = answers.stream().filter(ans -> options.stream().anyMatch(option -> option.equals(ans))).findFirst();
             if (optionalOption.isPresent()) {
@@ -42,27 +44,51 @@ public class DialogueUtils {
     }
 
     public static void talkTo(String npc) {
+        talkTo(npc, null);
+    }
+
+    public static void talkTo(String npc, List<String> answers) {
+        if(answers == null) answers = new ArrayList<>();
+
+        if (TutorialUtils.needsOpenTab()) {
+            TutorialUtils.handleTab();
+        }
+
+        Sleep.sleepUntil(Dialogues::canContinue, Calculations.random(500, 1200));
+        solve(answers);
+
+        if(Dialogues.canContinue()) {
+            Dialogues.continueDialogue();
+        } else {
+            NPC n = NPCs.closest(npc);
+            if (n != null) {
+                if (n.isOnScreen()) {
+                    if (n.interact("Talk-to")) {
+                        Sleep.sleepUntil(Dialogues::canContinue, Calculations.random(1200, 1600));
+                    }
+                } else {
+                    Walking.walk(n);
+                }
+            }
+        }
+
+        Sleep.sleepUntil(Dialogues::canContinue, Calculations.random(500, 1200));
+
+        /*
         if (!Dialogues.canContinue()) {
             final NPC guide = NPCs.closest(npc);
             if (guide != null) {
                 if (guide.isOnScreen()) {
                     if (guide.interact("Talk-to")) {
-                        dialogueSleep();
                         Sleep.sleepUntil(Dialogues::canContinue, Calculations.random(1200, 1600));
                     }
                 } else {
                     Walking.walk(guide);
-                    dialogueSleep();
                 }
             }
         } else {
             Dialogues.continueDialogue();
             sleep(200, 500);
-        }
-    }
-
-    private static void dialogueSleep() {
-        Sleep.sleepUntil(() -> Players.getLocal().isMoving(), Calculations.random(300, 1200));
-        Sleep.sleepUntil(() -> !Players.getLocal().isMoving(), Calculations.random(300, 1200));
+        }*/
     }
 }
