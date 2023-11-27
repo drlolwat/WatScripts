750f505168b5d7816d1e397275c726623d2b5c3a
lolwat <chchproud@gmail.com>
2023-11-27 02:19:10 -0500

    High level alchemy, banking changes

---
+++ WatAB/src/main/java/org/lolwat/tasks/types/magic/HighAlchemyTask.java
@@ -0,0 +1,167 @@
+package org.lolwat.tasks.types.magic;
+
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.combat.Combat;
+import org.dreambot.api.methods.combat.CombatStyle;
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.container.impl.bank.Bank;
+import org.dreambot.api.methods.container.impl.equipment.Equipment;
+import org.dreambot.api.methods.dialogues.Dialogues;
+import org.dreambot.api.methods.interactive.NPCs;
+import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.methods.magic.Magic;
+import org.dreambot.api.methods.magic.Normal;
+import org.dreambot.api.methods.magic.Spell;
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.quest.book.Quest;
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.skills.Skills;
+import org.dreambot.api.methods.tabs.Tab;
+import org.dreambot.api.methods.tabs.Tabs;
+import org.dreambot.api.utilities.Logger;
+import org.dreambot.api.utilities.Sleep;
+import org.dreambot.api.wrappers.interactive.NPC;
+import org.dreambot.api.wrappers.items.Item;
+import org.lolwat.WatAIO;
+import org.lolwat.misc.utils.GenericUtils;
+import org.lolwat.misc.utils.combat.magic.MagicUtils;
+import org.lolwat.misc.utils.combat.ranged.RangedUtils;
+import org.lolwat.tasks.WatTask;
+import org.lolwat.tasks.types.misc.BankingTask;
+import org.lolwat.tasks.types.misc.TraversalTask;
+
+import java.util.*;
+
+
+public class HighAlchemyTask implements WatTask {
+    private int minLevel;
+    private int maxLevel;
+    private String item;
+    boolean alched = false;
+
+    List<String> items = new ArrayList<String>() {
+        {
+            add("Steel chainbody");
+            add("Steel kiteshield");
+            add("Mithril med helm");
+            add("Steel full helm");
+            add("Steel warhammer");
+            add("Mithril dagger");
+        }
+    };
+
+    public HighAlchemyTask(int minimumLevel, int maximumLevel) {
+        minLevel = minimumLevel;
+        maxLevel = maximumLevel;
+        Collections.shuffle(items);
+        item = items.get(0);
+    }
+
+    @Override
+    public String getName() {
+        return "High level alchemy";
+    }
+
+    @Override
+    public boolean canPerformTask() {
+        return Skills.getRealLevel(Skill.MAGIC) >= minLevel && Skills.getRealLevel(Skill.MAGIC) <= maxLevel;
+    }
+
+    @Override
+    public void execute(WatAIO instance) {
+        int casts = Calculations.random(75, 150);
+        if (Skills.getRealLevel(Skill.MAGIC) >= 75) {
+            casts *= 2;
+        }
+
+        HashMap<String, Integer> requiredItems = new HashMap<>();
+        requiredItems.put("Coins", -1);
+        requiredItems.putAll(MagicUtils.getRunesRequired(Normal.HIGH_LEVEL_ALCHEMY, casts));
+        requiredItems.put(item, -casts);
+
+        if (!MagicUtils.canAffordCast(Normal.HIGH_LEVEL_ALCHEMY)) {
+            Logger.log("We need to grab runes...");
+            instance.currentTask = new BankingTask(null, requiredItems, null, 1, this);
+            return;
+        }
+
+        if (!Inventory.contains(item)) {
+            Logger.log("We need to grab HA target (" + item + ")");
+            instance.currentTask = new BankingTask(null, requiredItems, null, 1, this);
+            return;
+        }
+
+        if(Bank.isOpen()) {
+            Bank.close();
+            Sleep.sleep(100, 200);
+        }
+
+        if (Dialogues.canContinue()) {
+            Dialogues.continueDialogue();
+        }
+
+        if (!Tabs.isOpen(Tab.MAGIC)) {
+            Tabs.open(Tab.MAGIC);
+            Sleep.sleep(120, 240);
+        }
+
+        if (!Magic.castSpell(Normal.HIGH_LEVEL_ALCHEMY)) {
+            Logger.log("error casting HA");
+            return;
+        }
+
+        if (!Tabs.isOpen(Tab.INVENTORY)) {
+            Logger.log("inventory was not open..?");
+            return;
+        }
+
+        if (Inventory.interact(item)) {
+            alched = false;
+            Sleep.sleep(50, 100);
+            Sleep.sleepUntil(() -> alched && !Players.getLocal().isAnimating() && !Players.getLocal().isMoving(), Calculations.random(750, 1000));
+        } else {
+            Logger.log("issue interacting with " + item);
+        }
+    }
+
+    @Override
+    public boolean requiresLogin() {
+        return true;
+    }
+
+    @Override
+    public int loopTime() {
+        return 650;
+    }
+
+    @Override
+    public void onExpGained(Skill skill, int amount, WatAIO instance) {
+        alched = true;
+    }
+
+    @Override
+    public Skill trainsSkill() {
+        return Skill.MAGIC;
+    }
+
+    @Override
+    public Integer avoidAfterLevel() {
+        return maxLevel;
+    }
+
+    @Override
+    public Quest completesQuest() {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Integer> clothesRequired() {
+        return new HashMap<String, Integer>() {
+            {
+                putAll(GenericUtils.getSkillingGear());
+                put("Staff of fire", 1);
+            }
+        };
+
+    }
+}
