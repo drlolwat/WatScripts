fe24f16f81cfb63c66b872d413a2c724a86ba3b6
Unknown <unknown@unknown.com>
2023-12-01 12:52:29 -0500

    Fuck it

--- WatMarketAlcher/src/main/java/org/lolwat/tasks/types/quests/GoblinDiplomacyQuest.java
+++ WatMarketAlcher/src/main/java/org/lolwat/tasks/types/quests/GoblinDiplomacyQuest.java
@@ -103,14 +103,18 @@ public class GoblinDiplomacyQuest implements WatTask {
     }
 
     private boolean dyeMail(String color) {
-        if(Inventory.count("Goblin mail") > 0 && Inventory.contains(color + " dye")) {
-            if(Inventory.get(color + " dye").interact("Use")) {
-                Sleep.sleep(50, 200);
-                if (Inventory.get("Goblin mail").interact()) {
-                    Sleep.sleep(50, 200);
-                    return true;
+        if(Inventory.count("Goblin mail") == 3) {
+            while(Inventory.count("Goblin mail") > 1) {
+                if(Inventory.contains(color + " dye")) {
+                    if(Inventory.get(color + " dye").interact("Use")) {
+                        Sleep.sleep(50, 200);
+                        if(Inventory.get("Goblin mail").interact()) {
+                            Sleep.sleep(50, 200);
+                        }
+                    }
                 }
             }
+            return Inventory.count("Goblin mail") == 1;
         }
         return false;
     }
