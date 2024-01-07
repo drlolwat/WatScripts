647b27c44b2307cf0cbb902cc11d44c9aef52dee
lolwat <me@lolwat.net>
2024-01-07 13:36:35 -0500

    Address null pointer exception in MiningTask

--- WatScriptStarter/src/main/java/org/lolwat/tasks/types/mining/MiningTask.java
+++ WatScriptStarter/src/main/java/org/lolwat/tasks/types/mining/MiningTask.java
@@ -164,7 +164,7 @@ public class MiningTask implements WatTask {
                     gotRock = true;
                 } else {
                     Logger.log("traversing to closer rocks");
-                    instance.currentTask = new TraversalTask(GameObjects.all(n -> n.canReach() && n.getName().equals(rockName)).get(0).getTile().getArea(3), this);
+                    instance.currentTask = new TraversalTask(GameObjects.all(n -> n != null && n.canReach() && n.getName().equals(rockName)).get(0).getTile().getArea(3), this);
                     return;
                 }
 
