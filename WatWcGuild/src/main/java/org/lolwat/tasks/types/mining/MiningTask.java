da46d2a9d3cb5b50435bb29f0c43f0c654eb2342
lolwat <me@lolwat.net>
2024-01-07 12:13:30 -0500

    Detect if cannot reach rock in MiningTask and call a TraversalTask if required

--- WatWcGuild/src/main/java/org/lolwat/tasks/types/mining/MiningTask.java
+++ WatWcGuild/src/main/java/org/lolwat/tasks/types/mining/MiningTask.java
@@ -162,6 +162,11 @@ public class MiningTask implements WatTask {
                     }
 
                     gotRock = true;
+                } else {
+                    if(obj != null && !obj.canReach()) {
+                        instance.currentTask = new TraversalTask(obj.getTile().getArea(3), this);
+                        return;
+                    }
                 }
 
                 if (lastTile != null) {
