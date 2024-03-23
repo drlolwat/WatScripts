7e76e1f0563d804b49fa056eb46388e5f0ca52be
lolwat <chchproud@gmail.com>
2024-03-23 05:27:23 -0400

    ConfigManager: new option (boolean) - bury_bones

--- WatBlastMiner/src/main/java/org/lolwat/managers/ConfigManager.java
+++ WatBlastMiner/src/main/java/org/lolwat/managers/ConfigManager.java
@@ -69,6 +69,7 @@ public class ConfigManager {
         defaultProfile.addProperty("bond_min_ttl", 0);
         defaultProfile.addProperty("use_profile_cape", false);
         defaultProfile.addProperty("faster_quests", false);
+        defaultProfile.addProperty("pickup_bones", false);
 
         return defaultProfile;
     }
