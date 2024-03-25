a092b07b61c1ad013062fa9794adb9664646d7af
lolwat <chchproud@gmail.com>
2024-03-25 06:21:43 -0400

    Obstacle: add Area variable

--- WatScriptStarter/src/main/java/org/lolwat/tasks/types/agility/types/Obstacle.java
+++ WatScriptStarter/src/main/java/org/lolwat/tasks/types/agility/types/Obstacle.java
@@ -1,27 +1,33 @@
 package org.lolwat.tasks.types.agility.types;
 
+import org.dreambot.api.methods.map.Area;
+
 public class Obstacle {
-    private String name;
-    private String action;
+    private final String name;
+    private final String action;
+    private final Area backupArea;
 
     public Obstacle(String name, String action) {
         this.name = name;
         this.action = action;
+        this.backupArea = null;
+    }
+
+    public Obstacle(String name, String action, Area backup) {
+        this.name = name;
+        this.action = action;
+        this.backupArea = backup;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getAction() {
         return action;
     }
 
-    public void setAction(String action) {
-        this.action = action;
+    public Area getBackupArea() {
+        return backupArea;
     }
 }
