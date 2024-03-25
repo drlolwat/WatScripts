8318c4922b388483fafe9a7805fb0b0bdaf8967a
lolwat <chchproud@gmail.com>
2024-03-25 19:04:14 -0400

    TeleportItemUtils: change vars to final

--- WatBlastMiner/src/main/java/org/lolwat/misc/utils/TeleportItemUtils.java
+++ WatBlastMiner/src/main/java/org/lolwat/misc/utils/TeleportItemUtils.java
@@ -6,10 +6,10 @@ import org.dreambot.api.methods.container.impl.equipment.Equipment;
 import org.dreambot.api.wrappers.items.Item;
 
 public class TeleportItemUtils {
-    public static String RING_OF_DUELING = "Ring of dueling";
-    public static String LUMBRIDGE_TELEPORT = "Lumbridge teleport";
-    public static String FALADOR_TELEPORT = "Falador teleport";
-    public static String CAMELOT_TELEPORT = "Camelot teleport";
+    public static final String RING_OF_DUELING = "Ring of dueling";
+    public static final String LUMBRIDGE_TELEPORT = "Lumbridge teleport";
+    public static final String FALADOR_TELEPORT = "Falador teleport";
+    public static final String CAMELOT_TELEPORT = "Camelot teleport";
 
     public static String getTeleportForBank(BankLocation loc) {
         switch(loc) {
@@ -18,6 +18,7 @@ public class TeleportItemUtils {
                 return RING_OF_DUELING;
             }
 
+            case SEERS:
             case CATHERBY: {
                 return CAMELOT_TELEPORT;
             }
@@ -43,8 +44,22 @@ public class TeleportItemUtils {
     }
 
     public static String getChargedItemName(String item) {
-        if (item.equals(RING_OF_DUELING)) {
-            return "Ring of dueling(8)";
+        switch(item) {
+            case RING_OF_DUELING: {
+                return "Ring of dueling(8)";
+            }
+
+            case LUMBRIDGE_TELEPORT: {
+                return "Lumbridge teleport";
+            }
+
+            case FALADOR_TELEPORT: {
+                return "Falador teleport";
+            }
+
+            case CAMELOT_TELEPORT: {
+                return "Camelot teleport";
+            }
         }
 
         return "";
