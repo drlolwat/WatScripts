74a7db40a65faa2e938abdcd20f7ac9cd117607d
lolwat <chchproud@gmail.com>
2024-03-25 05:39:16 -0400

    Obstacle: new class for holding Agility obstacles

---
+++ WatShayzienCollector/src/main/java/org/lolwat/tasks/types/agility/types/Obstacle.java
@@ -0,0 +1,27 @@
+package org.lolwat.tasks.types.agility.types;
+
+public class Obstacle {
+    private String name;
+    private String action;
+
+    public Obstacle(String name, String action) {
+        this.name = name;
+        this.action = action;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
