60425e123ff593e4d1c263b876d4cfd73c49bf3a
Unknown <unknown@unknown.com>
2024-03-29 18:20:16 -0400

    Witch's Potion Quest added

--- WatSelfAlcher/src/main/java/org/lolwat/managers/TaskManager.java
+++ WatSelfAlcher/src/main/java/org/lolwat/managers/TaskManager.java
@@ -7,6 +7,7 @@ import org.dreambot.api.methods.map.Area;
 import org.dreambot.api.methods.map.Tile;
 import org.dreambot.api.methods.quest.Quests;
 import org.dreambot.api.methods.quest.book.FreeQuest;
+import org.dreambot.api.methods.quest.book.PaidQuest;
 import org.dreambot.api.methods.quest.book.Quest;
 import org.dreambot.api.methods.settings.PlayerSettings;
 import org.dreambot.api.methods.skills.Skill;
@@ -106,7 +107,7 @@ public class TaskManager {
             }
         } else {
             for (Map.Entry<Quest, WatTask> questTask : questTasks.entrySet()) {
-                if (questTask.getValue().canPerformTask() && Quests.isFinished(questTask.getValue().completesQuest())) {
+                if (questTask.getValue().canPerformTask() && !Quests.isFinished(questTask.getValue().completesQuest())) {
                     Logger.log("TaskManager: Selected quest task: " + questTask.getValue().getName());
                     setCurrentTask(questTask.getValue(), 0);
                     return;
@@ -458,6 +459,9 @@ public class TaskManager {
         tasks.put(FreeQuest.GOBLIN_DIPLOMACY, new GoblinDiplomacyQuest());
         tasks.put(FreeQuest.ROMEO_AND_JULIET, new RomeoJulietQuest());
         tasks.put(FreeQuest.THE_RESTLESS_GHOST, new TheRestlessGhostQuest());
+        tasks.put(FreeQuest.WITCHS_POTION, new WitchsPotionQuest());
+        //tasks.put(PaidQuest.DRUIDIC_RITUAL, new DruidicRitualQuest());
+        //tasks.put(PaidQuest.PRIEST_IN_PERIL, new PriestInPerilQuest());
 
         return tasks;
     }
