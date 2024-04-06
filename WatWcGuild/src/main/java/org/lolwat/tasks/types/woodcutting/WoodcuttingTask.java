b48e849604bbc1be2ff86968365066d031522570
lolwat <chchproud@gmail.com>
2024-04-06 12:30:55 -0400

    WoodcuttingTask: using new banking system

--- WatWcGuild/src/main/java/org/lolwat/tasks/types/woodcutting/WoodcuttingTask.java
+++ WatWcGuild/src/main/java/org/lolwat/tasks/types/woodcutting/WoodcuttingTask.java
@@ -59,7 +59,7 @@ public class WoodcuttingTask implements WatTask {
         if ((!Inventory.contains(hatchet) && !Equipment.contains(hatchet)) || (Inventory.contains(hatchet) && Inventory.get(hatchet).isNoted())) {
             WatConfig.incrementToolFailures();
             Logger.log("I don't own the best hatchet available for me: " + hatchet);
-            TaskManager.getInstance().setCurrentTask(new BankingTask(new HashMap<String, Integer>() { { put(hatchet, 1); }}, sellList, 1, this));
+            TaskManager.getInstance().setCurrentTask(new BankingTask(null, sellList, 1, this));
         } else {
             if (!Tab.INVENTORY.isOpen()) {
                 Tab.INVENTORY.open();
