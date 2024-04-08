f2784971f3fa011d3833684b3db33ae45649dab8
lolwat <chchproud@gmail.com>
2024-04-08 01:25:50 -0400

    ConfigManager: new entry skill cfg 'runecrafting'

--- WatShayzienCollector/src/main/java/org/lolwat/managers/ConfigManager.java
+++ WatShayzienCollector/src/main/java/org/lolwat/managers/ConfigManager.java
@@ -59,6 +59,7 @@ public class ConfigManager {
         defaultProfile.addProperty("mining", 99);
         defaultProfile.addProperty("agility", 1);
         defaultProfile.addProperty("herblore", 1);
+        defaultProfile.addProperty("runecrafting", 1);
 
         defaultProfile.addProperty("quests_enabled", true);
         defaultProfile.addProperty("breaks_enabled", true);
