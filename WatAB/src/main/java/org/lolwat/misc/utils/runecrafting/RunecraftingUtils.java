250af437af3349a0e5d897fc35ad13e73e7ee2be
lolwat <chchproud@gmail.com>
2024-04-08 01:24:22 -0400

    RunecraftingUtils: new util class

---
+++ WatAB/src/main/java/org/lolwat/misc/utils/runecrafting/RunecraftingUtils.java
@@ -0,0 +1,30 @@
+package org.lolwat.misc.utils.runecrafting;
+
+import org.dreambot.api.methods.magic.cost.Rune;
+import org.dreambot.api.methods.map.Area;
+
+public class RunecraftingUtils {
+    public static Area getRunecraftingAreaByRune(String rune) {
+        switch(rune) {
+            case "air":
+            default: {
+                return new Area(2982, 3296, 2990, 3288);
+            }
+            case "water": {
+                return new Area(3179, 3168, 3186, 3161);
+            }
+            case "earth": {
+                return new Area(3301, 3476, 3309, 3471);
+            }
+            case "fire": {
+                return new Area(3307, 3257, 3315, 3249);
+            }
+            case "body": {
+                return new Area(3049, 3448, 3056, 3441);
+            }
+            case "mind": {
+                return new Area(2979, 3517, 2986, 3510);
+            }
+        }
+    }
+}
