fd2072d489326da9a00d1596f1131aeb8615777c
lolwat <chchproud@gmail.com>
2024-04-15 04:52:06 -0400

    GatheringTask: support GroundItem and GameObject

--- WatMarketAlcher/src/main/java/org/lolwat/tasks/misc/GatheringTask.java
+++ WatMarketAlcher/src/main/java/org/lolwat/tasks/misc/GatheringTask.java
@@ -3,11 +3,13 @@ package org.lolwat.tasks.misc;
 import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.interactive.GameObjects;
 import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.methods.item.GroundItems;
 import org.dreambot.api.methods.map.Area;
 import org.dreambot.api.methods.skills.Skill;
 import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.utilities.Sleep;
 import org.dreambot.api.wrappers.interactive.GameObject;
+import org.dreambot.api.wrappers.items.GroundItem;
 import org.lolwat.managers.TaskManager;
 import org.lolwat.managers.types.WatTask;
 
@@ -16,14 +18,16 @@ public class GatheringTask implements WatTask {
     private final String inventoryName;
     private final int amount;
     private final Area location;
+    private final boolean ground;
     private final WatTask postTask;
 
-    public GatheringTask(String gameObject, String inventoryItem, Integer amountRequired, Area loc, WatTask post) {
+    public GatheringTask(String gameObject, String inventoryItem, Integer amountRequired, Area loc, boolean groundItem, WatTask post) {
         objectName = gameObject;
         inventoryName = inventoryItem;
         amount = amountRequired;
         location = loc;
         postTask = post;
+        ground = groundItem;
     }
 
     @Override
@@ -44,13 +48,24 @@ public class GatheringTask implements WatTask {
             return;
         }
 
-        GameObject i = GameObjects.closest(x -> x != null && x.exists() && x.getName().equals(objectName));
-        if(i != null) {
-            if(!i.interact()) {
-                Logger.log("Failed to interact with item: " + i.getName() + " (" + objectName + "?)");
-            }
+        if(!ground) {
+            GameObject i = GameObjects.closest(x -> x != null && x.exists() && x.getName().equals(objectName));
+            if (i != null) {
+                if (!i.interact()) {
+                    Logger.log("Failed to interact with item: " + i.getName() + " (" + objectName + "?)");
+                }
 
-            Sleep.sleepUntil(() -> !i.exists() && Players.getLocal().isStandingStill(), 5000);
+                Sleep.sleepUntil(() -> !i.exists() && Players.getLocal().isStandingStill(), 5000);
+            }
+        } else {
+            GroundItem i = GroundItems.closest(x -> x != null && x.exists() && x.getName().equals(objectName));
+            if (i != null) {
+                if (!i.interact("Take")) {
+                    Logger.log("Failed to interact with item: " + i.getName() + " (" + objectName + "?)");
+                }
+
+                Sleep.sleepUntil(() -> !i.exists() && Players.getLocal().isStandingStill(), 5000);
+            }
         }
     }
 
