a97299dd403ba19b9e2b7aa6743e9bb236397b88
lolwat <chchproud@gmail.com>
2024-04-15 04:51:32 -0400

    Obstacle: support object id

--- WatScriptStarter/src/main/java/org/lolwat/tasks/agility/types/Obstacle.java
+++ WatScriptStarter/src/main/java/org/lolwat/tasks/agility/types/Obstacle.java
@@ -6,17 +6,34 @@ public class Obstacle {
     private final String name;
     private final String action;
     private final Area backupArea;
+    private final int id;
 
     public Obstacle(String name, String action) {
         this.name = name;
         this.action = action;
         this.backupArea = null;
+        this.id = 0;
     }
 
     public Obstacle(String name, String action, Area backup) {
         this.name = name;
         this.action = action;
         this.backupArea = backup;
+        this.id = 0;
+    }
+
+    public Obstacle(int id, String action) {
+        this.name = "";
+        this.action = action;
+        this.backupArea = null;
+        this.id = id;
+    }
+
+    public Obstacle(int id, String action, Area backup) {
+        this.name = "";
+        this.action = action;
+        this.backupArea = backup;
+        this.id = id;
     }
 
     public String getName() {
@@ -29,5 +46,9 @@ public class Obstacle {
 
     public Area getBackupArea() {
         return backupArea;
+    }
+
+    public int getId() {
+        return id;
     }
 }
