cca218be0cbaf947abf0d88026de583d506ae036
lolwat <chchproud@gmail.com>
2024-08-18 12:19:49 -0400

    Quality of life / bug fixes

--- WatWcGuild/src/main/java/org/lolwat/tasks/combat/RangedCombatTask.java
+++ WatWcGuild/src/main/java/org/lolwat/tasks/combat/RangedCombatTask.java
@@ -136,17 +136,19 @@ public class RangedCombatTask implements WatTask {
         GenericUtils.handleSpecial();
 
         if (!Players.getLocal().isInCombat()) {
-            if (GenericUtils.tooManyPlayers(10, 3)) {
+            if(GenericUtils.tooManyPlayers(10, 5)) {
                 TaskManager.getInstance().setCurrentTask(new HopperTask(0, this));
                 return;
             }
 
             GenericUtils.handlePickup(new ArrayList<>());
 
-            NPC closestFriend = NPCs.closest(x -> x != null && x.exists() && x.getName().equalsIgnoreCase(name) && !x.isInCombat() && !x.isHealthBarVisible() && zone.contains(x));
-            if (closestFriend != null && !closestFriend.isInCombat() && !closestFriend.isHealthBarVisible() && closestFriend.interact("Attack")) {
-                GenericUtils.moveMouseInOrOut();
+            NPC closestFriend = NPCs.closest(x -> x != null && x.canReach() && x.exists() && x.getName().equalsIgnoreCase(name) && !x.isInCombat() && !x.isHealthBarVisible() && zone.contains(x));
+            if (closestFriend != null && !closestFriend.isInCombat() && !closestFriend.isHealthBarVisible() && !closestFriend.interact("Attack")) {
+                Logger.error("Error attacking NPC during MeleeCombatTask");
             }
+
+            Sleep.sleepUntil(() -> Players.getLocal().isInCombat(), Calculations.random(1500, 2500));
         }
     }
 
