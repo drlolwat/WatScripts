53a812c4a8f6818b0ca6a5e9c1ffba63adc94cdd
lolwat <chchproud@gmail.com>
2024-09-23 19:34:34 -0400

    initial commit

--- WatMarketAlcher/src/main/java/org/lolwat/misc/utils/StringUtils.java
+++
@@ -1,7 +0,0 @@
-package org.lolwat.misc.utils;
-
-public class StringUtils {
-    public static String capitalize(String str) {
-        return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase();
-    }
-}
