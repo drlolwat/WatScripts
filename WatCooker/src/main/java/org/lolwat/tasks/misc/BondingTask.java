4da3f2ccf1b34b309dc05422b5715fdda0cfa268
lolwat <chchproud@gmail.com>
2024-10-19 10:38:49 -0400

    BondingTask: new task

--- WatCooker/src/main/java/org/lolwat/tasks/misc/BondingTask.java
+++ WatCooker/src/main/java/org/lolwat/tasks/misc/BondingTask.java
@@ -1,43 +1,61 @@
 package org.lolwat.tasks.misc;
 
+import org.dreambot.api.Client;
 import org.dreambot.api.methods.Calculations;
 import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.container.impl.bank.Bank;
 import org.dreambot.api.methods.container.impl.bank.BankLocation;
+import org.dreambot.api.methods.dialogues.Dialogues;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.tabs.Tab;
+import org.dreambot.api.methods.tabs.Tabs;
+import org.dreambot.api.methods.widget.Widget;
 import org.dreambot.api.methods.widget.Widgets;
 import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.utilities.Sleep;
+import org.dreambot.api.wrappers.widgets.Menu;
 import org.dreambot.api.wrappers.widgets.WidgetChild;
-import org.lolwat.WatScript;
 import org.lolwat.managers.TaskManager;
 import org.lolwat.managers.types.WatTask;
+import org.lolwat.misc.utils.GenericUtils;
 
 import java.util.HashMap;
 
 public class BondingTask implements WatTask {
-    private final WatTask postTask;
-
     @Override
     public String getName() {
         return "Bonding";
     }
+    private WatTask post;
 
     public BondingTask(WatTask post) {
-        postTask = post;
+        this.post = post;
     }
 
     @Override
     public void execute() {
-        boolean requiresHop = false;
         if (!BankLocation.GRAND_EXCHANGE.getArea(5).contains(Players.getLocal())) {
             TaskManager.getInstance().setCurrentTask(new TraversalTask(BankLocation.GRAND_EXCHANGE.getArea(5), this));
             return;
         }
 
+        if(GenericUtils.isMember()) {
+            TaskManager.getInstance().setCurrentTask(post != null ? post : null);
+            return;
+        }
+
+        if (Menu.isMenuManipulationActive()) {
+            Logger.log("Disabling menu manipulation for bonding");
+            Menu.toggleMenuManipulation(false);
+        }
+
         if (!Inventory.contains("Old school bond (untradeable)")) {
-            //TaskManager.getInstance().setCurrentTask(new BankingTask(this));
+            TaskManager.getInstance().setCurrentTask(new BankingTask(new HashMap<String, Integer>() {
+                {
+                    put("Old school bond (untradeable)", 1);
+                }
+            }, null, 1, this, null));
             return;
         }
 
@@ -49,43 +67,59 @@ public class BondingTask implements WatTask {
         Sleep.sleepUntil(() -> !Bank.isOpen(), Calculations.random(500, 1000));
 
         if (!Inventory.interact("Old school bond (untradeable)", "Redeem")) {
-            Logger.error("Error redeeming bond, please report. #3");
+            Logger.error("Error redeeming bond, please report. #1");
             return;
         }
 
-        Sleep.sleepUntil(() -> Widgets.isVisible(861), Calculations.random(500, 1000));
 
-        if (Widgets.isVisible((861))) {
-            WidgetChild c = Widgets.getWidget(861).getChild(12);
-            if (c != null && c.getActions() != null) {
-                if (!c.interact()) {
-                    Logger.error("Error redeeming bond, please report. #1");
+        Sleep.sleepUntil(() -> Widgets.isVisible(861), 15000);
+        Widget bondWindow = Widgets.getWidget(861);
+
+        if (bondWindow != null && bondWindow.isVisible()) {
+            Logger.log("opened bond window");
+            WidgetChild singleBond = bondWindow.getChild(12);
+            if (singleBond != null && singleBond.getActions() != null) {
+                if (!singleBond.interact("14 days membership")) {
+                    Logger.error("Error redeeming bond, please report. #2");
                     return;
                 }
 
-                Sleep.sleepUntil(() -> Widgets.isVisible(289), Calculations.random(500, 1000));
-                if (Widgets.isVisible(289)) {
-                    WidgetChild a = Widgets.getWidget(289).getChild(8);
-                    if (a != null && a.getActions() != null) {
-                        if (!a.interact()) {
-                            Logger.error("Error redeeming bond, please report. #2");
+                Sleep.sleepUntil(() -> Widgets.isVisible(289), 15000);
+                Widget redeemWindow = Widgets.getWidget(289);
+
+                if (redeemWindow != null && redeemWindow.isVisible()) {
+                    WidgetChild acceptButton = redeemWindow.getChild(8);
+                    if (acceptButton != null && acceptButton.getActions() != null) {
+                        if (!acceptButton.interact("Accept")) {
+                            Logger.error("Error redeeming bond, please report. #3");
                             return;
                         }
-
-                        Sleep.sleep(5000, Calculations.random(10000, 15000));
-                        requiresHop = true;
                     }
                 }
             }
         }
 
-        if (requiresHop) {
-            TaskManager.getInstance().setCurrentTask(new HopperTask(0, postTask));
-            return;
+        Sleep.sleepUntil(Dialogues::canContinue, 25000);
+
+        if(!Inventory.contains("Old school bond (untradeable)")) {
+            if(Client.isLoggedIn()) {
+                if(!Tabs.isOpen(Tab.LOGOUT)) {
+                    Tabs.open(Tab.LOGOUT);
+                    Sleep.sleepUntil(() -> Tabs.isOpen(Tab.LOGOUT), 5000);
+                }
+
+                Tabs.logout();
+
+                if(post != null) {
+                    TaskManager.getInstance().setCurrentTask(post);
+                }
+            }
         }
 
-        Logger.error("For some reason, BondingTask did not require a hop.");
-        TaskManager.getInstance().setCurrentTask(postTask);
+        if (!Menu.isMenuManipulationActive()) {
+            Logger.log("Enabling menu manipulation, bonding complete");
+            Menu.toggleMenuManipulation(true);
+        }
     }
 
     @Override
@@ -96,11 +130,6 @@ public class BondingTask implements WatTask {
     @Override
     public int loopTime() {
         return 500;
-    }
-
-    @Override
-    public void onExpGained(Skill skill, int amount, WatScript instance) {
-
     }
 
     @Override
@@ -116,11 +145,6 @@ public class BondingTask implements WatTask {
     @Override
     public Integer avoidAfterLevel() {
         return 101;
-    }
-
-    @Override
-    public HashMap<String, Integer> clothesRequired() {
-        return new HashMap<>();
     }
 
     @Override
