82fd7fc9abf3e19aa4e9c565d75ea20b2bc4e4f0
lolwat <chchproud@gmail.com>
2025-01-02 20:02:41 -0500

    ClassifiedTask: new task

---
+++ WatAB/src/main/java/org/lolwat/tasks/crafting/GemCuttingTask.java
@@ -0,0 +1,99 @@
+package org.lolwat.tasks.crafting;
+
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.container.impl.bank.Bank;
+import org.dreambot.api.methods.dialogues.Dialogues;
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.skills.Skills;
+import org.dreambot.api.methods.widget.Widget;
+import org.dreambot.api.methods.widget.Widgets;
+import org.dreambot.api.utilities.Logger;
+import org.dreambot.api.utilities.Sleep;
+import org.dreambot.api.wrappers.widgets.WidgetChild;
+import org.lolwat.managers.TaskManager;
+import org.lolwat.managers.types.WatTask;
+import org.lolwat.misc.utils.crafting.CraftingUtils;
+import org.lolwat.tasks.misc.BankingTask;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class GemCuttingTask implements WatTask {
+    private int avoidAfterLevel = 0;
+    private int totalInventories = 1;
+
+    public GemCuttingTask(int avoidAfterLevel) {
+        this.avoidAfterLevel = avoidAfterLevel;
+        this.totalInventories = Calculations.random(10, 30);
+    }
+
+    @Override
+    public String getName() {
+        return "Gem cutting";
+    }
+
+    @Override
+    public void execute() {
+        for(Map.Entry<String, Integer> entry : inventoryRequired().entrySet()) {
+            if(!Inventory.contains(x -> x != null && !x.isNoted() && x.getName().equals(entry.getKey()))) {
+                Logger.log("Missing " + entry.getKey());
+                TaskManager.getInstance().setCurrentTask(new BankingTask(null, null, totalInventories, this, null));
+                return;
+            }
+        }
+
+        if(Bank.isOpen()) {
+            Bank.close();
+            return;
+        }
+
+        String cuttingGem = CraftingUtils.getBestGemToCut();
+
+        if(!Inventory.use("Chisel")) {
+            Logger.error("Could not use chisel");
+            return;
+        }
+
+        if(!Inventory.interact(x -> x != null && x.getName().equals(cuttingGem))) {
+            Logger.error("Could not cut gem");
+            return;
+        }
+
+        Sleep.sleepUntil(() -> Widgets.getWidget(270) != null && Objects.requireNonNull(Widgets.getWidget(270)).isVisible(), 5000);
+
+        Widget w = Widgets.getWidget(270);
+        if(w != null) {
+            WidgetChild c = w.getChild(14); // only ever 1
+            if(c != null) {
+                if(c.interact()) {
+                    Sleep.sleepUntil(() -> !Inventory.contains(cuttingGem) || Dialogues.canContinue(), 60000);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean canPerformTask() {
+        return Skills.getRealLevel(Skill.CRAFTING) < avoidAfterLevel;
+    }
+
+    @Override
+    public Skill trainsSkill() {
+        return Skill.CRAFTING;
+    }
+
+    @Override
+    public Integer avoidAfterLevel() {
+        return avoidAfterLevel;
+    }
+
+    @Override
+    public HashMap<String, Integer> inventoryRequired() {
+        HashMap<String, Integer> reqs = new HashMap<>();
+        reqs.put(CraftingUtils.getBestGemToCut(), 27);
+        reqs.put("Chisel", 1);
+        return reqs;
+    }
+}
