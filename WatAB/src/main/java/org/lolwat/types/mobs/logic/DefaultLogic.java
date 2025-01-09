5ffa9ef82d67d50dfd219358a0a06a9e3e5d362c
lolwat <lolwat@botbuddy.net>
2025-01-09 18:11:32 -0500

    DefaultLogic: new class: default logic for basic OSRS mobs

---
+++ WatAB/src/main/java/org/lolwat/types/mobs/logic/DefaultLogic.java
@@ -0,0 +1,13 @@
+package org.lolwat.types.mobs.logic;
+
+import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.utilities.Logger;
+import org.lolwat.types.interfaces.MobLogic;
+import org.lolwat.types.mobs.Mob;
+
+public class DefaultLogic implements MobLogic {
+    @Override
+    public void execute(Mob mob, Skill skill) {
+        Logger.log("executing mob logic " + this.getClass().getName() + " for " + mob.getName() + " for skill " + skill.getName());
+    }
+}
