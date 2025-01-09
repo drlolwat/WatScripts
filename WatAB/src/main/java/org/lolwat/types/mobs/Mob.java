0ccd00a514a72720b5910ae0d667b0654ca906e4
lolwat <lolwat@botbuddy.net>
2025-01-09 18:11:07 -0500

    Mob: new class

--- WatAB/src/main/java/org/lolwat/types/mobs/Mob.java
+++ WatAB/src/main/java/org/lolwat/types/mobs/Mob.java
@@ -1,9 +1,10 @@
-package org.lolwat.types;
+package org.lolwat.types.mobs;
 
 import lombok.Getter;
 import org.dreambot.api.methods.map.Area;
 import org.dreambot.api.methods.quest.book.Quest;
 import org.dreambot.api.methods.skills.Skill;
+import org.lolwat.types.interfaces.MobLogic;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,15 +16,21 @@ public class Mob {
     private final HashMap<Skill, Integer> levelRequirements;
     private final List<Quest> questRequirements;
     private final boolean membersOnly;
+    private final MobLogic mobLogic;
+
     // maybe rangedOnly or meleeOnly?
     // maybe a list of required gear, for slayer, the combat task can inherit from this class and have a list of required gear
 
-    public Mob(String name, HashMap<String, Area> locations, HashMap<Skill, Integer> levelRequirements, List<Quest> questRequirements, boolean membersOnly) {
+    public Mob(String name, HashMap<String, Area> locations, HashMap<Skill, Integer> levelRequirements, List<Quest> questRequirements, boolean membersOnly, MobLogic mobLogic) {
         this.name = name;
         this.locations = locations;
         this.levelRequirements = levelRequirements;
         this.questRequirements = questRequirements;
         this.membersOnly = membersOnly;
+        this.mobLogic = mobLogic;
     }
 
+    public Area getBestLocation() {
+        return locations.get("bullshit");
+    }
 }
