6ad1724a26cd4a7bb8c532470dab604b7c947903
lolwat <lolwat@botbuddy.net>
2025-01-09 21:42:34 -0500

    GearItem: bind price to item for G.E ops

--- WatAB/src/main/java/org/lolwat/types/gear/GearItem.java
+++ WatAB/src/main/java/org/lolwat/types/gear/GearItem.java
@@ -1,22 +1,28 @@
 package org.lolwat.types.gear;
 
 import lombok.Getter;
+import lombok.Setter;
+import org.lolwat.misc.utils.NumUtils;
 
 @Getter
 public class GearItem {
     private final String name;
     private final String searchFor;
     private final int quantity;
+    @Setter
+    private int price;
 
     public GearItem(String name, String searchFor, int quantity) {
         this.name = name;
         this.searchFor = searchFor;
         this.quantity = quantity;
+        this.price = NumUtils.getItemPrice(name);
     }
 
     public GearItem(String name, int quantity) {
         this.name = name;
         this.searchFor = name;
         this.quantity = quantity;
+        this.price = NumUtils.getItemPrice(name);
     }
 }
