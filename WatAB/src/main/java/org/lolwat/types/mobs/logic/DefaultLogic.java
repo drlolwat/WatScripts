f0d5d052888f9aada682a27bbced71e272b875c1
lolwat <lolwat@botbuddy.net>
2025-05-24 19:18:11 -0400

    DefaultLogic: prod

--- WatAB/src/main/java/org/lolwat/types/mobs/logic/DefaultLogic.java
+++ WatAB/src/main/java/org/lolwat/types/mobs/logic/DefaultLogic.java
@@ -1,13 +1,6 @@
 package org.lolwat.types.mobs.logic;
 
-import org.dreambot.api.methods.skills.Skill;
-import org.dreambot.api.utilities.Logger;
 import org.lolwat.types.interfaces.MobLogic;
-import org.lolwat.types.mobs.Mob;
 
 public class DefaultLogic implements MobLogic {
-    @Override
-    public void execute(Mob mob, Skill skill) {
-        Logger.log("executing mob logic " + this.getClass().getSimpleName() + " for " + mob.getName() + " for skill " + skill.getName());
-    }
 }
