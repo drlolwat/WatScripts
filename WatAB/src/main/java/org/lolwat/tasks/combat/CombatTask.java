90b795b36ecc9e04bda86b02836eb1f963839873
lolwat <lolwat@botbuddy.net>
2025-05-24 19:18:01 -0400

    CombatTask: gearing, inventory

--- WatAB/src/main/java/org/lolwat/tasks/combat/CombatTask.java
+++ WatAB/src/main/java/org/lolwat/tasks/combat/CombatTask.java
@@ -1,24 +1,30 @@
 package org.lolwat.tasks.combat;
 
+import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.container.impl.equipment.Equipment;
 import org.dreambot.api.methods.container.impl.equipment.EquipmentSlot;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.methods.skills.Skill;
 import org.dreambot.api.methods.skills.Skills;
 import org.dreambot.api.utilities.Logger;
+import org.dreambot.api.utilities.Sleep;
 import org.dreambot.api.wrappers.items.Item;
 import org.lolwat.managers.ConfigManager;
 import org.lolwat.managers.ItemManager;
 import org.lolwat.managers.TaskManager;
 import org.lolwat.misc.types.combat.CombatType;
 import org.lolwat.misc.utils.CombatUtils;
+import org.lolwat.misc.utils.WatUtils;
+import org.lolwat.tasks.banking.WithdrawSingleItemTask;
 import org.lolwat.tasks.combat.gearing.CombatGearTask;
 import org.lolwat.tasks.misc.WalkingTask;
+import org.lolwat.types.gear.WatItem;
 import org.lolwat.types.interfaces.WatTask;
 import org.lolwat.types.mobs.Mob;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class CombatTask implements WatTask {
     private final Skill skill;
@@ -44,6 +50,15 @@ public class CombatTask implements WatTask {
             return;
         }
 
+        if(!Players.getLocal().isInCombat()) {
+            Mob bestMob = CombatUtils.getBestMob(skill);
+            if (target == null || !target.equals(bestMob)) {
+                target = bestMob;
+                Logger.log("Switched to better mob: " + target.getName() + " for skill " + skill.getName());
+                return;
+            }
+        }
+
         // check for required items. we will do this custom
         List<EquipmentSlot> slotsMissing = new ArrayList<>();
         List<EquipmentSlot> slotsRequired = new ArrayList<>(target.getMobLogic().slotsRequired());
@@ -58,13 +73,60 @@ public class CombatTask implements WatTask {
             if(!Equipment.isSlotEmpty(s)) {
                 Item i = Equipment.getItemInSlot(s);
                 if(i != null) {
-                    if(!ItemManager.getInstance().isValidWeapon(i.getName(), type)) {
-                        Logger.log(i.getName() + " is not a valid weapon for this task");
-                        acceptable = false;
+                    if(s.equals(EquipmentSlot.WEAPON)) {
+                        if (!ItemManager.getInstance().isValidWeapon(i.getName(), type)) {
+                            Logger.log(i.getName() + " is not a valid weapon for this task");
+                            acceptable = false;
+                        }
+                    } else {
+                        if (!ItemManager.getInstance().isValidWearable(i.getName(), s, type)) {
+                            Logger.log(i.getName() + " is not a valid item for this task");
+                            acceptable = false;
+                        }
                     }
                 }
             } else {
                 acceptable = false;
+                for(Item i : Inventory.all()) {
+                    if(i == null || i.isNoted()) continue;
+                    if(s.equals(EquipmentSlot.WEAPON)) {
+                        if (ItemManager.getInstance().isValidWeapon(i.getName(), type)) {
+                            Logger.log("we have a valid weapon in inventory");
+                            if(!WatUtils.equipItem(i.getName(), null)) {
+                                Logger.error("error equipping weapon " + i.getName());
+                                return;
+                            }
+
+                            Sleep.sleepUntil(() -> Equipment.contains(i.getName()), 5000);
+
+                            if(!Equipment.contains(i.getName())) {
+                                Logger.error("error equipping weapon " + i.getName());
+                                return;
+                            }
+
+                            acceptable = true;
+                            break;
+                        }
+                    } else {
+                        if (ItemManager.getInstance().isValidWearable(i.getName(), s, type)) {
+                            Logger.log("we have a valid item in inventory");
+                            if(!WatUtils.equipItem(i.getName(), null)) {
+                                Logger.error("error equipping wearable " + i.getName());
+                                return;
+                            }
+
+                            Sleep.sleepUntil(() -> Equipment.contains(i.getName()), 5000);
+
+                            if(!Equipment.contains(i.getName())) {
+                                Logger.error("error equipping wearable " + i.getName());
+                                return;
+                            }
+
+                            acceptable = true;
+                            break;
+                        }
+                    }
+                }
             }
 
             if(!acceptable) {
@@ -77,6 +139,16 @@ public class CombatTask implements WatTask {
             Logger.log("we need to get equipment for " + slotsMissing.size() + " slots");
             TaskManager.getInstance().setCurrentTask(new CombatGearTask(this, type, slotsMissing));
             return;
+        }
+
+        if(!target.getMobLogic().inventoryLoadout().isEmpty()) {
+            for(Map.Entry<WatItem, Integer> map : target.getMobLogic().inventoryLoadout().entrySet()) {
+                if(!Inventory.contains(x -> x != null && x.getName().equalsIgnoreCase(map.getKey().getName()) && !x.isNoted())) {
+                    Logger.log("we need to get " + map.getKey().getName() + " x" + map.getValue());
+                    TaskManager.getInstance().setCurrentTask(new WithdrawSingleItemTask(map.getKey().getName(), map.getValue(), this));
+                    return;
+                }
+            }
         }
 
         //go to best location for mob
