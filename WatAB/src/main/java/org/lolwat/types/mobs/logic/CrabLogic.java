23f2be8125f550e15daad0814bb204b09802db16
lolwat <lolwat@botbuddy.net>
2025-05-27 00:01:41 -0400

    CrabLogic: add varrock, camelot teleport tabs to loadout

--- WatAB/src/main/java/org/lolwat/types/mobs/logic/CrabLogic.java
+++ WatAB/src/main/java/org/lolwat/types/mobs/logic/CrabLogic.java
@@ -26,7 +26,9 @@ public class CrabLogic implements MobLogic {
     public HashMap<WatItem, Integer> inventoryLoadout() {
         return new HashMap<WatItem, Integer>() {
             {
-                put(ItemManager.getInstance().getItem("Lobster"), 24);
+                put(ItemManager.getInstance().getItem("Lobster"), 20);
+                put(ItemManager.getInstance().getItem("Camelot teleport"), 2);
+                put(ItemManager.getInstance().getItem("Varrock teleport"), 2);
             }
         };
     }
