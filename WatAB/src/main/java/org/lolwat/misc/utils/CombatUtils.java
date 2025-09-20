fe18e1f67138dce9fff39dcd7d52afb78964ee61
lolwat <lolwat@botbuddy.net>
2025-09-20 15:01:32 -0400

    CombatUtils: better mob requirement filtering

--- WatAB/src/main/java/org/lolwat/misc/utils/CombatUtils.java
+++ WatAB/src/main/java/org/lolwat/misc/utils/CombatUtils.java
@@ -7,12 +7,26 @@ import org.dreambot.api.methods.skills.Skills;
 import org.lolwat.managers.MobManager;
 import org.lolwat.types.mobs.Mob;
 
+import java.util.Map;
+
 public class CombatUtils {
     private static boolean meetsRequirements(Mob mob, Skill skill) {
         if (mob.getLevelRequirements() != null) {
-            Integer req = mob.getLevelRequirements().get(skill);
-            if (req != null && Skills.getRealLevel(skill) < req) {
-                return false;
+            for (Map.Entry<Skill, Integer> map : mob.getLevelRequirements().entrySet()) {
+                Skill reqSkill = map.getKey();
+                if (skill == Skill.MAGIC && (isMeleeSkill(reqSkill) || reqSkill == Skill.RANGED)) {
+                    continue;
+                }
+                if (skill == Skill.RANGED && (isMeleeSkill(reqSkill) || reqSkill == Skill.MAGIC)) {
+                    continue;
+                }
+                if (isMeleeSkill(skill) && (reqSkill == Skill.MAGIC || reqSkill == Skill.RANGED)) {
+                    continue;
+                }
+
+                if (Skills.getRealLevel(reqSkill) < map.getValue()) {
+                    return false;
+                }
             }
         }
 
@@ -26,7 +40,6 @@ public class CombatUtils {
 
         return true;
     }
-
     public static Mob getBestMob(Skill skill) {
         Mob bestMob = null;
         int highestLevel = 0;
@@ -44,5 +57,9 @@ public class CombatUtils {
         }
 
         return bestMob;
+    }
+
+    private static boolean isMeleeSkill(Skill s) {
+        return s == Skill.ATTACK || s == Skill.STRENGTH || s == Skill.DEFENCE;
     }
 }
