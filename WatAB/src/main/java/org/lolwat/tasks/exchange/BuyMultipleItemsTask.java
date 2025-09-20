e2990309fba1bae461cd3835dec60a92f3c44663
lolwat <lolwat@botbuddy.net>
2025-09-20 15:36:05 -0400

    BuyMultipleItemsTask: fix not reprocessing items

--- WatAB/src/main/java/org/lolwat/tasks/exchange/BuyMultipleItemsTask.java
+++ WatAB/src/main/java/org/lolwat/tasks/exchange/BuyMultipleItemsTask.java
@@ -1,5 +1,6 @@
 package org.lolwat.tasks.exchange;
 
+import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.skills.Skill;
 import org.dreambot.api.utilities.Logger;
 import org.lolwat.managers.TaskManager;
@@ -39,6 +40,13 @@ public class BuyMultipleItemsTask implements WatTask {
         }
 
         if(processed.size() == items.size()) {
+            for(Map.Entry<WatItem, Integer> i : items.entrySet()) {
+                if(Inventory.count(x -> x.getName().contains(i.getKey().getName())) < i.getValue()) {
+                    processed.remove(i.getKey());
+                    return;
+                }
+            }
+
             Logger.log("processed all exchange items");
             TaskManager.getInstance().setCurrentTask(parent);
         }
